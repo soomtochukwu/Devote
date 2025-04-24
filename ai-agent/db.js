3bde69149aa9a940e4b4aec7a9b9219cff24b893
Pablo José Madrigal Quesada <22465189+pablomadrigal@users.noreply.github.com>
2025-04-23 23:23:46 -0600

    renaming folders (#66)

--- ai-agent/db.js
+++
@@ -1,6 +0,0 @@
-import Datastore from '@seald-io/nedb';
-
-const userDb = new Datastore({ filename: 'users.db', autoload: true });
-userDb.setAutocompactionInterval(60000); // compacts every 60 seconds
-
-export { userDb };
